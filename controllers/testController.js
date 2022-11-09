354f4f942bee3a1b4437e86ff0bdd26ab5745173
Joaquin-Garola <joaquingarola75@gmail.com>
2022-11-08 21:26:52 -0300

    new version

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,14 +1,12 @@
-const { faker }= require('@faker-js/faker');
-
 const generateProducts = (req, res) => {
   try{
     const products=[];
     for (let i=0; i<5; i++){
       const prod={
       id:i+1,
-      product_name:faker.commerce.productName(),
-      product_price:faker.commerce.price(100,5000,0,'$'),
-      product_thumbnail:faker.image.image(100, 100, true)
+      product_name:"Producto",
+      product_price: 7.5 * i,
+      product_thumbnail: "https://picsum.photos/100"
     }
       products.push(prod);
     }
